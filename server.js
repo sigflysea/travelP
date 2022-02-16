c9b58f2839d3937782b8ab6a77417f73df1a3034
sigflysea <junfree@gmail.com>
2022-02-16 14:46:15 -0600

    added models

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const app = express();
 
 connectDB();
 
+app.use("/api/users", require("./routes/users"));
+app.use("/api/profile", require("./routes/profile"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/posts", require("./routes/posts"));
+
 app.get("/", (req, res) => res.send("API up"));
 const PORT = process.env.PORT || 5000;
 
