a36e93a16baac4bbc3439aaa4ed11006bfe0ce0f
sigflysea <junfree@gmail.com>
2022-02-18 21:50:47 -0600

    token returned

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
 const bcrybt = require("bcryptjs");
+const jwttoken = require("jsonwebtoken");
+const config = require("config");
 const { check, validationResult } = require("express-validator");
 const User = require("../../models/User");
 //@route    POST api/users
@@ -36,8 +38,18 @@ router.post(
       user = new User({ name, email, avatar, password });
       const salt = await bcrybt.genSalt(10);
       user.password = await bcrybt.hash(password, salt);
-      await user.save();
-      res.send("ho dd");
+      await user.save(); // promise returns doc saved with id
+      const payload = { user: { id: user.id } };
+
+      jwttoken.sign(
+        payload,
+        config.get("jwtS"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (error) {
       console.error(error.message);
       res.status(500).send("server error");
